b86e5be63567dae955215da65089f1a6abbbf944
mildmojo <theaterfire@yahoo.com>
2013-09-15 17:03:05 -0400

    Implements trouble code querying (barely).

--- obd2_blacklist.rb
+++ obd2_blacklist.rb
@@ -9,3 +9,30 @@ while err do
   obd, err = OBD.connect "/dev/rfcomm0", 9600
   sleep 1
 end
+
+
+while true
+  response = obd['03']
+
+  if response.length <= 2
+    STDERR.puts 'short response'
+    next
+  end
+
+  codes = []
+  response = response[2..-1]
+  until response.empty?
+    codes << response[0..3]
+    response = response[4..-1]
+  end
+
+  if codes.all? { |code| blacklist.include?(code) }
+    clear_codes
+  end
+
+  sleep 5
+end
+
+def clear_codes
+  puts 'OH NOES NOT ENOUGH TIME'
+end
